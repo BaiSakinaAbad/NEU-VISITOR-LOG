bd96a54057051c23594ccab99bdc3129cbc7c5a9
Bai Sakina B. Abad <baisakina.abad@neu.edu.ph>
2026-03-01 17:33:16 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@/

---
+++ src/lib/data.ts
@@ -0,0 +1,12 @@
+export const affiliations = [
+  "College of Engineering",
+  "College of Science",
+  "College of Arts, Media and Design",
+  "College of Social Sciences and Humanities",
+  "D'Amore-McKim School of Business",
+  "Khoury College of Computer Sciences",
+  "Bouvé College of Health Sciences",
+  "School of Law",
+  "College of Professional Studies",
+  "Other",
+];
