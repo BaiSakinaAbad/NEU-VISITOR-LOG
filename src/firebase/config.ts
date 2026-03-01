87e37ccb4d0ad7ba1996a7bf7677e294c62c1e5f
Bai Sakina B. Abad <baisakina.abad@neu.edu.ph>
2026-03-01 17:09:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8220934128-d1b3c",
+  "appId": "1:278984140198:web:73dc934610e6b7459e9b67",
+  "apiKey": "AIzaSyAJJmxVpUDvQJEEt4hbBea_j_AO_Li42zQ",
+  "authDomain": "studio-8220934128-d1b3c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "278984140198"
+};
