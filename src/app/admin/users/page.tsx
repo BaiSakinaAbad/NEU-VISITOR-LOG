e666e202c3c890f259fafc57babb888f328c7adb
Bai Sakina B. Abad <baisakina.abad@neu.edu.ph>
2026-03-07 07:21:10 +0000

    Try fixing this error: `Runtime ReferenceError: AlertDialogTrigger is no

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -35,6 +35,7 @@ import {
     AlertDialogFooter,
     AlertDialogHeader,
     AlertDialogTitle,
+    AlertDialogTrigger,
   } from "@/components/ui/alert-dialog"
   
 import { MoreHorizontal, Search, UserX } from "lucide-react";
